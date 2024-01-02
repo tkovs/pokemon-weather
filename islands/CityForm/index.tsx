baf65a311076832eb27336442c9ec8975ce3a9b4
tkovs <tkovs.silva@gmail.com>
2024-01-02 04:39:36 -0300

    feat: update CityForm to work with new signals state

--- islands/CityForm/index.tsx
+++ islands/CityForm/index.tsx
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "preact/hooks";
-import { Response, getWeatherData } from "../../services/weather.ts";
+import { city } from '../contexts/Pokemon/state.ts'
 
 const CityForm = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState<Response | undefined>(undefined)
-
-  const handleOnSubmit = async (e: any) => {
+  const handleOnSubmit = (e: any) => {
     e.preventDefault()
-    setIsLoading(true)
 
-    const city = e.target.elements.cityInput.value
-    const response = await getWeatherData(city)
-
-    setIsLoading(false)
-    setData(response)
+    const input = e.target.elements.cityInput.value
+    city.value = input
   }
-
-  useEffect(() => {
-    if (data) {
-      console.log({ data })
-    }
-  }, [data])
 
   return (
     <form onSubmit={handleOnSubmit}>
       <label htmlFor="cityInput">City:</label>
-      <input disabled={isLoading} type="text" id="cityInput" />
-      <button disabled={isLoading} type="submit">{isLoading ? 'Loading...' : 'Submit'}</button>
+      <input type="text" id="cityInput" />
+      <button type="submit">Submit</button>
     </form>
   )
 }
