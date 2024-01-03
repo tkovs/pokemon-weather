11b99e56858c915e97fbf58e394ad8d3d10d8d48
tkovs <tkovs.silva@gmail.com>
2024-01-03 01:55:20 -0300

    style: change button text color to white

--- islands/CityForm/index.tsx
+++ islands/CityForm/index.tsx
@@ -12,7 +12,7 @@ const CityForm = () => {
     <form onSubmit={handleOnSubmit} className="bg-white rounded-3xl h-10 w-full flex items-center overflow-hidden border-solid border-[#2e6fb6] border-2">
       <input type="text" id="cityInput" className="mx-4 bg-transparent h-full w-full focus:outline-none" placeholder="Type your city name" />
 
-      <button type="submit" className="text-[#ffcb03] bg-[#2e6fb6] hover:bg-[#1c5798] h-full px-4 font-bold text-lg">Search</button>
+      <button type="submit" className="text-white bg-[#2e6fb6] hover:bg-[#1c5798] h-full px-4 font-bold text-lg">Search</button>
     </form>
   )
 }
